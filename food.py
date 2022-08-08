35057c900cb5fa6a84ea40cf0ab762e950c0646e
Chandan <chandancvp08@gmail.com>
2022-08-08 19:08:35 +0530

    initial commit

---
+++ food.py
@@ -0,0 +1,74 @@
+import psycopg2
+from flask import Flask, jsonify, request
+
+conn = psycopg2.connect(database="hackathon", user='postgres', password='Welcome1', host='database-1.ce9vh9gmxb9m.ap-south-1.rds.amazonaws.com', port= '5432')
+cursor = conn.cursor()
+#establishing the database connection
+data = cursor.execute("select version()")
+data = cursor.fetchone()
+print("Connection established to: ", data)
+
+def setup():
+    # cursor.execute("DROP TABLE IF EXISTS food")
+    # conn.commit()
+
+    cursor.execute("""
+                CREATE TABLE IF NOT EXISTS food(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR (20),
+                location VARCHAR (2000),
+                quantity INTEGER
+                )
+                """
+    )
+    conn.commit()
+
+setup()
+
+
+
+def get_row(id):
+    cursor.execute("SELECT * FROM food WHERE id = $1",id)
+    data = cursor.fetchall()
+    return data
+
+def get_all():
+    cursor.execute("SELECT * FROM food")
+    data = cursor.fetchall()
+    print(data)
+    return data
+
+
+def new_entry(name,quantity,location):
+    cursor.execute("INSERT INTO food(name,quantity,location) VALUES(%s,%s,%s)",(name,quantity,location))
+    conn.commit()
+
+# new_entry("test",1,"Bangalore")
+
+  
+# creating a Flask app
+app = Flask(__name__)
+  
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+        data =  get_all()
+        return jsonify({'data': data})
+  
+  
+# A simple function to calculate the square of a number
+# the number to be squared is sent in the URL when we use GET
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10
+# this returns 100 (square of 10)
+@app.route('/home/<int:num>', methods = ['GET'])
+def disp(num):
+    return jsonify({'data': num**2})
+  
+  
+# driver function
+if __name__ == '__main__':
+    #app.run(debug = True)
+    app.run(debug = True)
